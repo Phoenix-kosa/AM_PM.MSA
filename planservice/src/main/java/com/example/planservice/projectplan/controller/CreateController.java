4d8bac242dac0f36dd21bcacf787e09911f1112a
insiu <insu1124@gmail.com>
2023-12-27 14:43:48 +0900

    MSA 변환 완료

---
+++ planservice/src/main/java/com/example/planservice/projectplan/controller/CreateController.java
@@ -0,0 +1,23 @@
+package com.example.planservice.projectplan.controller;
+
+import com.example.planservice.projectplan.service.ProjectPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping
+public class CreateController {
+
+    @Autowired
+    private ProjectPlanService projectPlanService;
+
+    @PostMapping("/api/project")
+    public ResponseEntity<?> createDefaultProjectPlans(@RequestParam("projectId") int projectId) {
+        projectPlanService.createDefaultProjectPlans(projectId);
+        return ResponseEntity.ok().build();
+    }
+
+}
